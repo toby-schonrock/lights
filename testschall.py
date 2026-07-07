9630d3f288428acb7c9bf97e7a01a90f9ed4b377
Tobias Schonrock <tobias@schonrocks.com>
2026-07-07 16:53:23 +0200

    testschall flashing added

--- testschall.py
+++ testschall.py
@@ -5,8 +5,17 @@ import lib.arion_lights as light
 import lib.artnetcontroller as anc
 
 controller = anc.ArtNetController("192.168.1.169")
+threadkill = light.spawn_update_thread(controller.send_packet)
 
-state = 255
+for _ in range(10):
+    light.panels.a.g = 255
+    time.sleep(0.5)
+    light.panels.a.g = 0
+    time.sleep(0.5)
+
+
+threadkill()
+
 while True:
     for i in range(360):
         for panel in light.panels:
